68f363252d9c4cdceba32494fda16da8e46e69e1
marizeraus <38335557+marizeraus@users.noreply.github.com>
2018-06-19 20:15:13 -0300

    new version of the game

--- jogo.py
+++ jogo.py
@@ -4,128 +4,167 @@ from PPlay.gameimage import *
 from PPlay.sprite import *
 import gerador
 
-def move_ship(ship, janela, teclado):
+WINDOW_HEIGHT = 700
+WINDOW_WIDTH = 630
+pontuacao = 0
+
+def move_ship(ship, teclado, time):
+    #funcao para movimentar a nave seguindo o input do teclado
     vel_ship=190
-    if ship.x>-2*ship.width/3 and ship.x<janela.width+ship.width/3:
+
+    if ship.x>-2*ship.width/3 and ship.x<WINDOW_WIDTH+ship.width/3:
         if teclado.key_pressed('LEFT'):
-            ship.x-=vel_ship*janela.delta_time()
+            ship.x-=vel_ship*time
         if teclado.key_pressed('RIGHT'):
-            ship.x+=vel_ship*janela.delta_time()
-    elif ship.x>janela.width+ship.width/3:
+            ship.x+=vel_ship*time
+
+    elif ship.x>WINDOW_WIDTH+ship.width/3:
         ship.x=0-5
+
     else:
-        ship.x=janela.width
+        ship.x=WINDOW_WIDTH
 
-def shoot(shots, teclado, ship,i):
-    shots[i].set_position(ship.x+ship.width/2-1.5, ship.y-ship.height/3)
-    shots[i].draw()
 
-def move_shot(i, shots, janela):
-    vel_shot=200
-    shots[i].y-=vel_shot*janela.delta_time()
-    shots[i].draw()
+def move_monsters(monsters, virou, count_turn, monster_speed, time, fase):
+    #funcao para movimentar os monstros
+    clean_monsters(monsters)
+    if virou:
+        count_turn+=1
+        if count_turn==40:
+            virou = False
+            count_turn=0
+    else:
+        virou = should_i_stay_or_should_i_turn(monsters)
+        count_turn = 0
+        if virou:
+            monster_speed=-monster_speed
 
-def stop_shot(shots,i):
-    if shots[i].y<-10:
-        shots.pop(i)
-
-def move_monsters(monsters,time, virou, vel_monster, count_turn):
     for i in range(len(monsters)):
-        if len(monsters[i])>0:
-            if monsters[i][0].x<0 or (monsters[i][0].x+monsters[i][0].width)>janela.width or (monsters[i][-1].x)<0 or (monsters[i][-1].x+monsters[i][-1].width) >janela.width:
-                if not virou[i]:
-                    vel_monster[i]=-vel_monster[i]
-                    virou[i]=True
-                    for j in range(len(monsters[i])):
-                        monsters[i][j].y+=10
-            if virou:
-                count_turn[i]+=1
-                if count_turn[i] == 15:
-                    virou[i]=False
-                    count_turn[i]=0
-            for j in range(len(monsters[i])):
-                monsters[i][j].x+=vel_monster[i]*time
-    return virou, vel_monster, count_turn
+        for j in range(len(monsters[i])):
+            if virou and count_turn==0:
+                monsters[i][j].y+=10
+            monsters[i][j].x+=monster_speed*time*fase/2
 
-def kill_monster(monsters, shots, i):
-    for c in range(i):
-        if shots[i].y<janela.height/2:
-            print('aaa')
-            for j in range(len(monsters)-1,-1,-1):
-                for w in range(len(monsters[j])):
-                    if shots[c].collided(monsters[j][w]):
-                        shots.pop(c)
-                        monsters[j][w].x=100000000
-                        monsters[j].pop(w)
-                        break
+    return count_turn, virou, monster_speed
 
 
+def clean_monsters(monsters):
+    count=0
+    for linha in monsters:
+        if len(linha)==0:
+            monsters.pop(count)
+        else:
+            count+=1
 
-def did_you_win(monsters):
-    ganhou=True
+def should_i_stay_or_should_i_turn(monsters):
+    #identifica se os monstros devem virar
+
     for i in range(len(monsters)):
-        if len(monsters[i])!=0:
-            ganhou = False
-    return ganhou
+        if monsters[i][0].x<0 or (monsters[i][0].x+monsters[i][0].width)>WINDOW_WIDTH or (monsters[i][-1].x)<0 or (monsters[i][-1].x+monsters[i][-1].width) >WINDOW_WIDTH:
+            return True
 
-def game(dif):
-    conta_tempo=0
+    return False
+
+
+def move_shot(tiros_dados, time, monsters):
+    clean_shots(tiros_dados)
+    for i in range(len(tiros_dados)):
+        tiros_dados[i].y-=200*time
+        tiros_dados[i].draw()
+        stop_shot(tiros_dados , i)
+        kill_monster(monsters,tiros_dados, i)
+        i+=1
+
+def clean_shots(tiros_dados):
+    count=0
+    for i in tiros_dados:
+        if i.y>10000:
+            tiros_dados.pop(count)
+        else:
+            count+=1
+
+def shoot(tiros_disponiveis, tiros_dados, ship):
+    if len(tiros_disponiveis)>0:
+        tiros_dados.append(tiros_disponiveis[0])
+        tiros_disponiveis.pop(0)
+        tiros_dados[-1].set_position(ship.x+ship.width/2-1.5, ship.y-ship.height/3)
+        tiros_dados[-1].draw()
+
+
+def stop_shot(tiros_dados, i):
+    if tiros_dados[i].y<-10:
+        tiros_dados[i].y=100000
+
+
+
+def kill_monster(monsters, tiros_dados, i):
+    global pontuacao
+    for j in range(len(monsters)):
+        for w in range(len(monsters[j])):
+            if tiros_dados[i].collided(monsters[j][w]):
+                tiros_dados[i].y=100000
+                monsters[j][w].x=100000
+                monsters[j].pop(w)
+                pontuacao+=1
+                print(pontuacao)
+                break
+
+
+def game(dif, fase, vidas):
+    janela = Window(630,700)
     janela.clear()
-    janela.set_title('Jogo')
-    janela.set_background_color((0,0,0))
+    janela.set_title('Space Invaders')
     teclado = Window.get_keyboard()
-    shots, count=gerador.generate_shots(dif)
+
+# preparar os game objects
+    fundo = GameImage('images/fundo2.png')
+    monsters = gerador.generate_monsters(fase)
+    monster_speed = 220*dif/3
+    tiros_dados = []
+    tiros_disponiveis, contagem_de_tiros = gerador.generate_shots(dif)
     ship = GameImage('images/ship.png')
     ship.set_position(janela.width / 2 - ship.width, janela.height - ship.height - 10)
+
+    #variaveis para serem usadas durante o jogo
+    virou = False
+    count_turn = 0
     foi = False
-    count=0
-    monsters = gerador.generate_monsters(dif)
-    virou=[False]*len(monsters)
-    count_turn=[0]*len(monsters)
-    vel_monster=[30]*len(monsters)
+    count_shoot = 0
+
     for j in range(len(monsters)):
         for i in range(len(monsters[j])):
             monsters[j][i].set_position(10+52*i,44*j)
             monsters[j][i].set_sequence_time(0,2,500,True)
-
     while True:
         janela.update()
-        janela.set_background_color((0,0,0))
+        fundo.draw()
         ship.draw()
-        move_ship(ship,janela,teclado)
+        time = janela.delta_time()
         for i in range(len(monsters)):
             for j in range(len(monsters[i])):
                 monsters[i][j].draw()
                 monsters[i][j].update()
-
-        if teclado.key_pressed('space') and conta_tempo==0:
-            shoot(shots, teclado, ship,count)
-            foi=True
-            count+=1
+        if teclado.key_pressed('SPACE') and count_shoot==0:
+            shoot(tiros_disponiveis, tiros_dados, ship)
+            foi = True
+            count_shoot+=time
         if foi:
-            conta_tempo+=janela.delta_time()
-            if conta_tempo>0.4:
-                conta_tempo=0
+            count_shoot+=time
+            if count_shoot>0.4:
                 foi = False
-        for i in range(count):
-            shots[i].draw()
-            move_shot(i,shots, janela)
-            #kill_monster(monsters, shots, i)
-        virou, vel_monster, count_turn=move_monsters(monsters, janela.delta_time(), virou, vel_monster, count_turn)
-        if did_you_win(monsters):
-            print('woooooooo')
-
-
-janela = Window(630,700)
-janela.set_title('Space Invaders')
-janela.set_background_color((0,0,0))
-
-
-#o que fazer:
-#pra mexer, usar só um virou, que vira todos de uma vez só
-#o monstro mais a direita e o mais a esquerda são o parametro
-#1) tiro do player matar monstros e otimizar
-#2) caso o player mate todos os monstros, muda a fase
-#3) placar
-#4) tiro do monstro(aleatorio)
-#5) 3 vidas para o player, perde vida se tomar tiro, game over se monstros chegarem no fundo
+                count_shoot=0
+        if len(tiros_dados)>0:
+            move_shot(tiros_dados,time, monsters)
+        move_ship(ship, teclado, time)
+        count_turn, virou, monster_speed = move_monsters(monsters,virou, count_turn, monster_speed, time, fase)
+        if len(monsters)==0:
+            if fase==6:
+                win()
+            else:
+                game(dif, fase+1, vidas)
+            break
+    if vidas==0:
+        arq=open('ranking.txt', 'a')
+        nome=input()
+        pont=str(pontuacao)+' '+nome+'/n'
+        arq.close()
